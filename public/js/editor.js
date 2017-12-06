8a572db00cf58260bc5cda95dd684c96039eb668
Lensual <lenvisual@gmail.com>
2017-12-07 04:37:53 +0800

    update markdown2html viewer

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,9 +1,23 @@
 
 $(function() {
-    var editor = editormd("editormd", {
-        path : "/public/editor.md/lib/",
-        width: "95%",
-        height: "50%",
-        saveHTMLToTextarea: true
-    });
+    if(document.getElementById("editormd")){
+        var editor = editormd("editormd", {
+            path : "/public/editor.md/lib/",
+            width: "95%",
+            height: "50%",
+            saveHTMLToTextarea: true,
+        });
+    }
+
+    if(document.getElementById("viewermd")){
+        var viewer = editormd.markdownToHTML("viewermd", {
+            htmlDecode: true,
+            emoji           : true,
+            taskList        : true,
+            tex             : true,  // 默认不解析
+            flowChart       : true,  // 默认不解析
+            sequenceDiagram : true,  // 默认不解析
+        });
+    }
+
 });
